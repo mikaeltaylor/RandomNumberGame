a2a45e83e707130e9d994f635903174ba910324e
mikaeltaylor <mikael.e.taylor@gmail.com>
2016-05-16 07:52:40 -0500

    Random Number Game Commit

---
+++ RandomNumberGame/RandomNumberForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RandomNumberGame
+{
+    partial class RandomNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGuessNumber = new System.Windows.Forms.Label();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnNewNumber = new System.Windows.Forms.Button();
+            this.btnGiveUp = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblNumGuesses = new System.Windows.Forms.Label();
+            this.lblNumGuessesResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGuessNumber
+            // 
+            this.lblGuessNumber.AutoSize = true;
+            this.lblGuessNumber.Location = new System.Drawing.Point(43, 18);
+            this.lblGuessNumber.Name = "lblGuessNumber";
+            this.lblGuessNumber.Size = new System.Drawing.Size(182, 13);
+            this.lblGuessNumber.TabIndex = 0;
+            this.lblGuessNumber.Text = "Guess a number between 1 and 100.";
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(46, 35);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(179, 20);
+            this.txtUserInput.TabIndex = 1;
+            this.txtUserInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUserInput_KeyPress);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(95, 72);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnNewNumber
+            // 
+            this.btnNewNumber.Location = new System.Drawing.Point(75, 101);
+            this.btnNewNumber.Name = "btnNewNumber";
+            this.btnNewNumber.Size = new System.Drawing.Size(121, 23);
+            this.btnNewNumber.TabIndex = 3;
+            this.btnNewNumber.Text = "New Number";
+            this.btnNewNumber.UseVisualStyleBackColor = true;
+            this.btnNewNumber.Click += new System.EventHandler(this.btnNewNumber_Click);
+            // 
+            // btnGiveUp
+            // 
+            this.btnGiveUp.Location = new System.Drawing.Point(95, 130);
+            this.btnGiveUp.Name = "btnGiveUp";
+            this.btnGiveUp.Size = new System.Drawing.Size(75, 23);
+            this.btnGiveUp.TabIndex = 4;
+            this.btnGiveUp.Text = "Give Up!";
+            this.btnGiveUp.UseVisualStyleBackColor = true;
+            this.btnGiveUp.Click += new System.EventHandler(this.btnGiveUp_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblResult.Location = new System.Drawing.Point(0, 189);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(264, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumGuesses
+            // 
+            this.lblNumGuesses.AutoSize = true;
+            this.lblNumGuesses.Location = new System.Drawing.Point(82, 156);
+            this.lblNumGuesses.Name = "lblNumGuesses";
+            this.lblNumGuesses.Size = new System.Drawing.Size(0, 13);
+            this.lblNumGuesses.TabIndex = 6;
+            // 
+            // lblNumGuessesResult
+            // 
+            this.lblNumGuessesResult.AutoSize = true;
+            this.lblNumGuessesResult.Location = new System.Drawing.Point(126, 169);
+            this.lblNumGuessesResult.Name = "lblNumGuessesResult";
+            this.lblNumGuessesResult.Size = new System.Drawing.Size(0, 13);
+            this.lblNumGuessesResult.TabIndex = 7;
+            // 
+            // RandomNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 202);
+            this.Controls.Add(this.lblNumGuessesResult);
+            this.Controls.Add(this.lblNumGuesses);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnGiveUp);
+            this.Controls.Add(this.btnNewNumber);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.lblGuessNumber);
+            this.Name = "RandomNumberForm";
+            this.Text = "Random Number Game v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGuessNumber;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnNewNumber;
+        private System.Windows.Forms.Button btnGiveUp;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblNumGuesses;
+        private System.Windows.Forms.Label lblNumGuessesResult;
+    }
+}
+
---
+++ RandomNumberGame/RandomNumberForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberGame
+{//begin game
+    public partial class RandomNumberForm : Form
+    {//begin form
+        int guessCount = 0;
+        //Initialize the variable guessCount to 0, as no guesses have been made yet.
+        int randomNumber;
+        //Initialize randomNumber variable.
+        int userInput;
+        //Initialize userInputVariable
+        Random random = new Random();
+        //Initialize random variable.
+        public RandomNumberForm()
+        {
+            InitializeComponent();
+            //Initialize the components of the form.
+            randomNumber = random.Next(1, 100);
+            //Choose a random number for the game.
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+            //When the submit button is clicked..
+        {
+            try
+                //Try the following
+            {
+                guessCount++;
+                //Increase guess number by one, as the submit button is how we'll count the number of guesses.
+                userInput = int.Parse(txtUserInput.Text);
+                //Convert the user input to a variable that we can compare against.
+                if (userInput == randomNumber)
+                    //If the user input is the same as our random number..
+                {
+                    lblResult.Text = "You chose the correct number! It was indeed "+ randomNumber+"!";
+                    //Change the result label's text to the appropriate message
+                    randomNumber = random.Next(1, 100);
+                    //Choose a new random number
+                    lblNumGuesses.Text = "Number of Guesses:"; 
+                    lblNumGuessesResult.Text = guessCount.ToString();
+                    //Change the guess count labels to the correct messages.
+                    guessCount = 0;
+                    //Reset the guess count
+                }//end if correct
+                else if (userInput > randomNumber && userInput < 100 && userInput > 0)
+                    //If the user input was in range and is higher than the random number
+                {
+                    lblResult.Text = "Your guess was too high!";
+                    //Display the appropriate message
+                }//end else if higher
+                else if (userInput < randomNumber && userInput < 100 && userInput > 0)
+                //If the user input was in range and is lower than the random number..
+                {
+                    lblResult.Text = "Your guess was too low!";
+                    //Display the appropriate message.
+                }//end else if lower
+                else if (userInput < 0 || userInput > 100)
+                    //If the user input was out of range..
+                {
+                    MessageBox.Show("Please enter a valid, non-negative whole number between 1 and 100.");
+                    //Prompt the user for another guess.
+                    if (guessCount > 0)
+                    {//begin if the guesscount is greater than zero
+                        guessCount--;
+                        //subtract the invalid guess
+                    }//end if
+                    else
+                    {//begin else
+                        guessCount = 0;
+                        //reset the guess count to 0
+                    }//end else
+                    //Subtract the invalid guess from the total guess count, or keep it at 0 if no guess has been made prior.
+                }//end else if
+            }
+            catch (Exception ex)
+            {
+                if (string.IsNullOrWhiteSpace(txtUserInput.Text))
+                    //If the user input is null..
+                {
+                    MessageBox.Show("You must enter a valid guess in order to play.  Please enter a valid, non-negative whole number between 1-100");
+                    //Display an appropriate error message
+                    if (guessCount > 0)
+                    {//begin if the guesscount is greater than zero
+                        guessCount--;
+                        //subtract the invalid guess
+                    }//end if
+                    else
+                    {//begin else
+                        guessCount = 0;
+                        //reset the guess count to 0
+                    }//end else
+                    //Subtract the invalid guess from the total guess count, or keep it at 0 if no guess has been made prior.                
+                }//end blank user input error
+                else
+                    //If any other error is caught
+                {
+                    MessageBox.Show("Error Message: " + ex);
+                    //Display the error message so we can figure out what to do with it.
+                    if (guessCount > 0)
+                    {//begin if the guesscount is greater than zero
+                        guessCount--;
+                        //subtract the invalid guess
+                    }//end if
+                    else
+                    {//begin else
+                        guessCount = 0;
+                        //reset the guess count to 0
+                    }//end else
+                    //Subtract the invalid guess from the total guess count, or keep it at 0 if no guess has been made prior.
+                }//end any other error
+            }//end catch
+        }
+
+        private void btnNewNumber_Click(object sender, EventArgs e)
+        {   //If the New Number button is clicked..
+            guessCount = 0;
+            //Set the guess count to 0.  With a new number, we should have a new count.
+            randomNumber = random.Next(0, 100);
+            //Pick a new random number.
+            lblResult.Text = "";
+            //Clear the result label.
+        }
+
+        private void btnGiveUp_Click(object sender, EventArgs e)
+            //If the user clicks the Give Up button..
+        {
+   lblResult.Text = "Your random number was " + randomNumber+".";
+            //Let them know what the number was
+            //Don't clear anything, because..  we wanted to be nice and let someone have a win.
+            //Also don't change the random number, because they haven't asked us to explicitly. New Number
+            //button exists for that purpose.
+        }
+
+        private void txtUserInput_KeyPress(object sender, KeyPressEventArgs e)//On keypress within the user input text box
+        {
+            //Allow control characters, e.g. backspace
+            if (Char.IsControl(e.KeyChar)) return;
+            //Allow numbers, but not letters.
+            if (e.KeyChar < '0' || e.KeyChar > '9')
+            {
+                MessageBox.Show("Please enter a valid, non-negative whole number between 1 and 100.");
+                e.KeyChar = (char)0;
+            }//end allow numbers, not letters
+        }//end keypress
+    
+
+    }//end form
+}//end game
